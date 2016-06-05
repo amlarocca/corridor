89243db6d3444d17c6f40e0644cc4936421a2a19
Anthony LaRocca <alarocca@Anthonys-MacBook-Pro.local>
2016-06-05 13:06:48 -0700

    so much bug

--- html/corridor.js
+++ html/corridor.js
@@ -423,7 +423,9 @@ function renderBoard()
     document.getElementById("playerNumber").textContent=(player_number + 1);
     updateStatus("Player " + (current_board.current_player + 1) + "'s Turn");
     renderWallSelector();
-    if (current_board.winner) {
+    if (current_board.winner === player_numer) {
+        updateStatus("You win!")
+    }else if (current_board.winner)
         updateStatus("Player " + (current_board.winner + 1) + " Wins!")
     }
 }
